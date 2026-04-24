f3f864343f8cf3ca75ced462ab0a5b38022954d2
brlim2183 <brlim2183@gmail.com>
2026-04-24 08:18:32 +0000

    done

--- lib/src/main/java/org/APCSLowell/APLine.java
+++ lib/src/main/java/org/APCSLowell/APLine.java
@@ -2,7 +2,11 @@ package org.APCSLowell;
 
 public class APLine {
     int a, b, c;
-    public APLine(int a, int b, int c) {}
+    public APLine(int a1, int b2, int c3) {
+        a=a1;
+        b=b2;
+        c=c3;
+    }
     public boolean isOnLine(int x, int y) {
         return a * x + b * y + c == 0;
     }
